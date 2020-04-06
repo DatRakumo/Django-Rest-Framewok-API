8e83c6dcb3fc576a3f0894390d55448d802d49ad
DatRakumo <datnv@rakumo.vn>
2020-04-06 09:40:55 +0700

    [6_4_2020] Seconrd commit

--- course/serializers.py
+++ course/serializers.py
@@ -2,7 +2,12 @@ from .models import Course
 from rest_framework import serializers
 
 
-class GetAllCourseSerialier(serializers.HyperlinkedModelSerializer):
+class GetAllCourseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Course
         fields = ['id', 'title']
+
+class CourseSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    price = serializers.IntegerField(default=0)
+    content = serializers.CharField(max_length=255)
--- course/views.py
+++ course/views.py
@@ -6,15 +6,24 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Course
-from .serializers import GetAllCourseSerialier
+from .serializers import GetAllCourseSerializer, CourseSerializer
 
 
 # Create your views here.
 
 class GetAllCourseAPIView(APIView):
 
-    def get(self,request):
+    def get(self, request):
         list_course = Course.objects.all()
-        mydata = GetAllCourseSerialier(list_course, many=True)
+        mydata = GetAllCourseSerializer(list_course, many=True)
         return Response(data=mydata.data, status=status.HTTP_200_OK)
 
+    def post(self, request):
+        mydata = CourseSerializer(data=request.data)
+        if not mydata.is_valid():
+            return Response("sai du lieu roi ", status=status.HTTP_400_BAD_REQUEST)
+        title = mydata.data['title']
+        price = mydata.data['price']
+        content = mydata.data['content']
+        cs = Course.objects.create(title=title, price=price, content=content)
+        return Response(data=cs.id, status=status.HTTP_200_OK)
